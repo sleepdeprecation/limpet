17b6433d6ab851b2c36ed4d48864318ec80b7947
Don Kuntz <dkuntz@journera.com>
2021-05-06 09:49:54 -0500

    don't run docker in interactive mode

--- package.sh
+++ package.sh
@@ -5,6 +5,6 @@ set -x -e
 VERSION=`grep '^version' Cargo.toml | cut -d '"' -f 2`
 echo "Packaging version ${VERSION}..."
 cargo clean
-docker run --rm -v `pwd`:/volume -ti clux/muslrust cargo build --release
+docker run --rm -v `pwd`:/volume clux/muslrust cargo build --release
 zip -j limpet_${VERSION}_linux_amd64.zip  target/x86_64-unknown-linux-musl/release/limpet
 
