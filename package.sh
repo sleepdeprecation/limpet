6d7085a9b031291b76707b2e68634bf56b59faab
Sleep Kuntz <sleep@kuntz.co>
2023-12-20 15:10:52 -0600

    okay try cross compiling but better?

--- package.sh
+++ package.sh
@@ -6,7 +6,7 @@ VERSION=`grep '^version' Cargo.toml | cut -d '"' -f 2`
 echo "Packaging version ${VERSION}..."
 cargo clean
 
-docker run --rm -v "$(pwd)":/home/rust/src messense/rust-musl-cross:armv7-musleabihf cargo build --release --target x86_64-unknown-linux-musl --target aarch64-unknown-linux-musl
+docker -c colima-intel run --rm -v "$(pwd)":/volume clux/muslrust:stable bash -c "~/.cargo/bin/rustup target add aarch64-unknown-linux-musl && cargo build --release --target x86_64-unknown-linux-musl --target aarch64-unknown-linux-musl"
 
 zip -j limpet_${VERSION}_linux_amd64.zip  target/x86_64-unknown-linux-musl/release/limpet
 zip -j limpet_${VERSION}_linux_arm64.zip target/aarch64-unknown-linux-musl/release/limpet
