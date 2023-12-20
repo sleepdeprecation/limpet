adf3cff63721d3975d1c42040aee4c7d14519223
Sleep Kuntz <sleep@kuntz.co>
2023-12-20 14:22:09 -0600

    use rustup to build x86 and arm binaries

--- package.sh
+++ package.sh
@@ -2,9 +2,14 @@
 
 set -x -e
 
+rustup target add aarch64-unknown-linux-musl
+rustup target add x86_64-unknown-linux-musl
+
 VERSION=`grep '^version' Cargo.toml | cut -d '"' -f 2`
 echo "Packaging version ${VERSION}..."
 cargo clean
-docker run --rm -v `pwd`:/volume clux/muslrust cargo build --release
-zip -j limpet_${VERSION}_linux_amd64.zip  target/x86_64-unknown-linux-musl/release/limpet
 
+cargo build --release --target x86_64-unknown-linux-musl --target aarch64-unknown-linux-musl
+
+zip -j limpet_${VERSION}_linux_amd64.zip  target/x86_64-unknown-linux-musl/release/limpet
+zip -j limpet_${VERSION}_linux_arm64.zip target/aarch64-unknown-linux-musl/release/limpet
