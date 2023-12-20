0d8a11d08c0e091903595bab5d58927169bcdc15
Sleep Kuntz <sleep@kuntz.co>
2023-12-20 14:43:59 -0600

    use docker container to build

--- package.sh
+++ package.sh
@@ -2,14 +2,11 @@
 
 set -x -e
 
-rustup target add aarch64-unknown-linux-musl
-rustup target add x86_64-unknown-linux-musl
-
 VERSION=`grep '^version' Cargo.toml | cut -d '"' -f 2`
 echo "Packaging version ${VERSION}..."
 cargo clean
 
-cargo build --release --target x86_64-unknown-linux-musl --target aarch64-unknown-linux-musl
+docker run --rm -it -v "$(pwd)":/home/rust/src messense/rust-musl-cross:armv7-musleabihf cargo build --release --target x86_64-unknown-linux-musl --target aarch64-unknown-linux-musl
 
 zip -j limpet_${VERSION}_linux_amd64.zip  target/x86_64-unknown-linux-musl/release/limpet
 zip -j limpet_${VERSION}_linux_arm64.zip target/aarch64-unknown-linux-musl/release/limpet
