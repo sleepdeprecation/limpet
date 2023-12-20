f91ffc30d9d4216983fd247e2fd41548ef0ec9fc
Sleep Kuntz <sleep@kuntz.co>
2023-12-20 15:11:40 -0600

    don't use the local context name i have in the actual build script

--- package.sh
+++ package.sh
@@ -6,7 +6,7 @@ VERSION=`grep '^version' Cargo.toml | cut -d '"' -f 2`
 echo "Packaging version ${VERSION}..."
 cargo clean
 
-docker -c colima-intel run --rm -v "$(pwd)":/volume clux/muslrust:stable bash -c "~/.cargo/bin/rustup target add aarch64-unknown-linux-musl && cargo build --release --target x86_64-unknown-linux-musl --target aarch64-unknown-linux-musl"
+docker run --rm -v "$(pwd)":/volume clux/muslrust:stable bash -c "~/.cargo/bin/rustup target add aarch64-unknown-linux-musl && cargo build --release --target x86_64-unknown-linux-musl --target aarch64-unknown-linux-musl"
 
 zip -j limpet_${VERSION}_linux_amd64.zip  target/x86_64-unknown-linux-musl/release/limpet
 zip -j limpet_${VERSION}_linux_arm64.zip target/aarch64-unknown-linux-musl/release/limpet
